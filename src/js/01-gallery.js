b9028d7cc3e10e877f2eb023ff883d1041d3cd05
Yurpalova-Tatiana <105441084+Yurpalova-Tatiana@users.noreply.github.com>
2022-10-02 18:09:24 +0300

    добавляет 1з

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,32 @@
-
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// console.log(SimpleLightbox);
 
-console.log(galleryItems);
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+
+
+//console.log(galleryItems[0].original);
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML = createGalleryMarkup(galleryItems);
+function createGalleryMarkup(params) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox('.gallery .gallery__link', {
+    captionsData: 'alt',
+    captionDelay:250,
+    /* options */
+});
